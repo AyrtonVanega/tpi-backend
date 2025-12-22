870a4f27ed2865b12d3c117e138c0d38131ca926
Ayrton <95386@sistemas.frc.utn.edu.ar>
2025-12-22 11:21:17 -0300

    SecurityConfig ServicioSolicitudes

---
+++ servicio-solicitudes/src/main/java/ar/edu/utn/frc/backend/solicitudes/config/SolicitudesSecurityConfig.java
@@ -0,0 +1,51 @@
+package ar.edu.utn.frc.backend.solicitudes.config;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SolicitudesSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated())
+            .oauth2ResourceServer(oauth2 -> 
+                oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
+        return http.build();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Bean
+    public Converter<Jwt, AbstractAuthenticationToken> jwtAuthenticationConverter() {
+        return jwt -> {
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+            Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+            if (realmAccess != null && realmAccess.containsKey("roles")) {
+                var roles = (Collection<String>) realmAccess.get("roles");
+                roles.stream()
+                    .map(r -> "ROLE_" + r.toUpperCase())
+                    .map(SimpleGrantedAuthority::new)
+                    .forEach(authorities::add);
+            }
+            return new JwtAuthenticationToken(jwt, authorities);
+        };
+    }
+}
