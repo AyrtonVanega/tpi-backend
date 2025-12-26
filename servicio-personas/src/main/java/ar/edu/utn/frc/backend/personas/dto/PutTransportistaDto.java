c1852988bc9339d57d9cdfa33ce2558a1228f6af
Ayrton <95386@sistemas.frc.utn.edu.ar>
2025-12-26 18:37:18 -0300

    correccion atributos PutTransportistaDto

--- servicio-personas/src/main/java/ar/edu/utn/frc/backend/personas/dto/PutTransportistaDto.java
+++ servicio-personas/src/main/java/ar/edu/utn/frc/backend/personas/dto/PutTransportistaDto.java
@@ -10,8 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PutTransportistaDto {
-    private String docTransportista;
-    private String tipoDocTransportista;
     private String nombre;
     private String apellido;
     private String telefono;
